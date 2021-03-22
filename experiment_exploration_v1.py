93bf63b3b31dd7b127b2049dfb5b1569779799d5
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-22 16:32:39 +0530

    updates

--- experiment_exploration_v1.py
+++ experiment_exploration_v1.py
@@ -9,12 +9,10 @@ import os
 from evaluate_reservoir import *
 from utilis import *
 from args_emg import args as my_args
-# from ax import optimize
 import pandas as pd
 from itertools import product
 import time
 from encode import *
-
 
 if __name__ == '__main__':
 
@@ -22,8 +20,6 @@ if __name__ == '__main__':
 	print(args.__dict__)
 	logging.basicConfig(level=logging.DEBUG)
 	logger = logging.getLogger(__name__)
-
-
 
 	df = pd.DataFrame({"memoryless":[],
 						"win":[],
@@ -65,19 +61,19 @@ if __name__ == '__main__':
 		dataset=['5_class']
 		,memoryless=[True]
 		,win=['1.0 * rand()']
-		,input_connection_density=[0.15]
+		,input_connection_density=[0.05,0.1,0.15,0.2]
 		,tstart = [0]
 		,tlast = [1800]
 		,freeze_time_ms = [0]
 		,thr_init = [1]
 		,thr_init_dev = [0.5]
 		,refractory=[1]
-        ,learning_algorithm=['critical','none']
-        ,topology = ['random', 'small-world']
+        ,learning_algorithm=['critical']
+        ,topology = ['small-world']
         ,lr_critical = [0.1]
         ,macrocolumnShape=[[2,2,2]]
-        , minicolumnShape=[[4,4,2]]
-        ,connection_density=[0.1,0.2,0.3,0.4,0.5,0.6]
+        ,minicolumnShape=[[4,4,4]]
+        ,connection_density=[0.1,0.2,0.3]
         ,adaptiveProb=[1]
         ,excitatoryProb=[0.8]
         ,noise=[0]
@@ -86,10 +82,11 @@ if __name__ == '__main__':
 		,wmax = [1]
 		,winitmax=[0.25]
 		,winitmin=[0]
+		,fold=[1]
     )
 	param_values = [v for v in parameters.values()]
 
-	for args.dataset,args.memoryless_flag, args.win,args.input_connection_density,args.tstart,args.tlast,args.freeze_time_ms,args.init_thr, args.init_thr_dev,args.refractory, args.learning_algorithm,args.topology,args.lr_critical, args.macrocolumnShape,args.minicolumnShape, args.connection_density, args.adaptiveProb,args.excitatoryProb,args.noise, args.stdp_tau, args.stdp_apre, args.wmax, args.winitmax, args.winitmin in product(*param_values):
+	for args.dataset,args.memoryless_flag, args.win,args.input_connection_density,args.tstart,args.tlast,args.freeze_time_ms,args.init_thr, args.init_thr_dev,args.refractory, args.learning_algorithm,args.topology,args.lr_critical, args.macrocolumnShape,args.minicolumnShape, args.connection_density, args.adaptiveProb,args.excitatoryProb,args.noise, args.stdp_tau, args.stdp_apre, args.wmax, args.winitmax, args.winitmin,args.fold in product(*param_values):
 
 			# Fix the seed of all random number generator
 		seed = int(args.seed)
