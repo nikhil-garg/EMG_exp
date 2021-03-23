e5db8306bdd85b1d50fd928dd8d0d6d08336a150
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-24 00:10:59 +0530

    updates

--- baseline_exploration.py
+++ baseline_exploration.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
                          })
 
 	parameters = dict(
-		dataset = [ '5_class']
+		dataset = [ '3_class','5_class']
 		,threshold = [0.5]
 		,interpfact = [5]
 		,refractory = [1]
